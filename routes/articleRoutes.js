b346ecdfa83b1302f18f50032d16d79823653ad6
Rodolphe <dutchr@live.fr>
2023-07-04 16:03:51 +0200

    :sparkles: Comments works finally !!

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -3,13 +3,24 @@ const {
   comments,
   applaud,
 } = require("../controllers/articleController");
+const Article = require("../models/articleModel");
 
 const router = require("express").Router();
 
-router.get('/create', (req,res) =>{
-  res.render('create')
-})
+router.get("/create", (req, res) => {
+  res.render("create");
+});
 router.post("/create", create);
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const id = req.params.id;
+  const article = await Article.findById({_id:id});
+  res.render("comments", { article });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+  
+});
 router.post("/:id/comments", comments);
 router.post("/:id/applaud", applaud);
 
